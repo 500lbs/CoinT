9206e0a9267143ad8c0ef0e0ffbf962ef42f6c08
500lbs <stevenksteffen@gmail.com>
2022-11-01 21:47:25 -0400

    UI manager

--- CoinToss/Assets/_Main/Scripts/UIManager.cs
+++ CoinToss/Assets/_Main/Scripts/UIManager.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Toolbox ToolBoxRef;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Player1Guess();
+        Player2Guess();
+        TotalFlipped();     
     }
+
+    void Player1Guess()
+    {
+        /* UPDATE UGUI WITH:
+         * public int player1Wins;
+         * public int player1Black;
+         * public int player1White;
+        */
+    }
+
+    void Player2Guess()
+    {
+        /* UPDATE UGUI WITH:
+         * public int player2Wins;
+         * public int player2Black;
+         * public int player2White;
+        */
+    }
+
+    void CheckStatus()
+    {
+        /* UPDATE UGUI WITH:
+         * Who's turn it is
+        */
+    }
+    void TotalFlipped()
+    {
+        // How many times has the coin been flipped?
+    }
+
 }
